9794c3b779d5c8efd0daa37128c123c577884b7a
Adam Cross <adamscross04@gmail.com>
2012-11-07 20:38:10 +0000

    completely changed everything Signed-off-by: Adam Cross <adamscross04@gmail.com>

--- public/index.php
+++ public/index.php
@@ -1,74 +1,29 @@
 <?php
-/**
- * Set error reporting and display errors settings.  You will want to change these when in production.
- */
-error_reporting(-1);
-ini_set('display_errors', 1);
-
-/**
- * Website document root
- */
-define('DOCROOT', __DIR__.DIRECTORY_SEPARATOR);
-
-/**
- * Path to the application directory.
- */
-define('APPPATH', realpath(__DIR__.'/../fuel/app/').DIRECTORY_SEPARATOR);
-
-/**
- * Path to the default packages directory.
- */
-define('PKGPATH', realpath(__DIR__.'/../fuel/packages/').DIRECTORY_SEPARATOR);
-
-/**
- * The path to the framework core.
- */
-define('COREPATH', realpath(__DIR__.'/../fuel/core/').DIRECTORY_SEPARATOR);
-
-// Get the start time and memory for use later
-defined('FUEL_START_TIME') or define('FUEL_START_TIME', microtime(true));
-defined('FUEL_START_MEM') or define('FUEL_START_MEM', memory_get_usage());
-
-// Boot the app
-require APPPATH.'bootstrap.php';
-
-// Generate the request, execute it and send the output.
-try
-{
-	$response = Request::forge()->execute()->response();
-}
-catch (HttpNotFoundException $e)
-{
-	$route = array_key_exists('_404_', Router::$routes) ? Router::$routes['_404_']->translation : Config::get('routes._404_');
-
-	if($route instanceof Closure)
-	{
-		$response = $route();
+	
+	require_once("../code/classes/classes.php");
+	$root = new Grepotools();
+	
+	if (isset($_GET['view']) && isset($_GET['controller'])) {
+		$controller = "Controller_".ucfirst($_GET['controller']);
+		$action = "action_".$_GET['view'];
 		
-		if( ! $response instanceof Response)
-		{
-			$response = Response::forge($response);
+		if (method_exists ($controller, $action)) {
+			if (isset($_GET['data']) && $_GET['data']) {
+				$controller::$action($_GET['data']);
+			} else {
+				$controller::$action();
+			}
+			
+		} else {
+			Director::error_404();	
 		}
-	}
-	elseif ($route)
-	{
-		$response = Request::forge($route, false)->execute()->response();
-	}
-	else
-	{
-		throw $e;
-	}
-}
+	} elseif (($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']) == str_replace(array("http://", "https://"), "", Grepotools::$config->base_url."/")) {
+		//Director::error_404();
 
-// This will add the execution time and memory usage to the output.
-// Comment this out if you don't use it.
-$bm = Profiler::app_total();
-$response->body(
-	str_replace(
-		array('{exec_time}', '{mem_usage}'),
-		array(round($bm[0], 4), round($bm[1] / pow(1024, 2), 3)),
-		$response->body()
-	)
-);
+		Grepotools::home();
+	} else {
+		Director::error_404();
+	}
 
-$response->send(true);
+
+?>
